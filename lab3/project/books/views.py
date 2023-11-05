ae7a82b72571e72885c8a549848eddba43ff0c15
L-ED <lyginegor00@gmail.com>
2023-11-05 22:00:09 +0300

    added login

--- lab3/project/books/views.py
+++ lab3/project/books/views.py
@@ -3,9 +3,11 @@ from django.urls import reverse
 from django.http import HttpResponse, HttpRequest, Http404, HttpResponseRedirect
 from .models import Book
 from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class IndexView(generic.ListView):
+class IndexView(LoginRequiredMixin, generic.ListView):
     template_name = "books/index.html"
     context_object_name = "books_list"
 
@@ -14,16 +16,12 @@ class IndexView(generic.ListView):
         return Book.objects.order_by("-publish_date")
 
 
-# def details(request, book_id):
-#     book = get_object_or_404(Book, pk=book_id)
-#     return render(request, "books/details.html", {"book": book})
-
-
-class DetailView(generic.DetailView):
+class DetailView(LoginRequiredMixin, generic.DetailView):
     model = Book
     template_name = "books/detail.html"
+    
 
-
+@login_required
 def vote(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     if request.POST.get("Add_fav")== "Add to favorites":
